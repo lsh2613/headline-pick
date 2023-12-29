23161162c8cc49b32ca6b050a64a4be5d4fb3e0e
LSH <104637774+lsh2613@users.noreply.github.com>
2023-12-29 11:57:11 +0900

    fix

--- src/main/java/news/Main.java
+++ src/main/java/news/Main.java
@@ -1,9 +1,6 @@
 package news;
-
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
